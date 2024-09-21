65262c4853c6fb949fb48a035128caf4934fec92
ab-dauletkhan <ab.dauletkhan@gmail.com>
2024-09-21 13:32:49 +0500

    just the basics

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 // handler function to the home page
@@ -18,10 +20,22 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet..."))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Display a specific snippet with ID = %d", id)
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	// We want to create only with POST method, otherwise we return error
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", "POST")
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
 	w.Write([]byte("Create a new snippet..."))
 }
 
